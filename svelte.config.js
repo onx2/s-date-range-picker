67e5d92ee7793683b37627149cbd9f5e4ea8bcb9
Jeff Rooks <rooksjj91@gmail.com>
2019-12-21 02:28:52 -0500

    code cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -13,27 +13,28 @@ const env = createEnv()
 module.exports = {
   preprocess: {
     style: async ({ content, attributes }) => {
-        if (attributes.type !== "text/scss" && attributes.lang !== "scss") return // lang is now taken into account
+      if (attributes.type !== "text/scss" && attributes.lang !== "scss") return // lang is now taken into account
 
-        return new Promise((resolve, reject) => {
-          sass.render({
-              data: content,
-              includePaths: ["src"],
-              sourceMap: true,
-              outFile: "x" // this is necessary, but is ignored
-            },
-            (err, result) => {
-              if (err) return reject(err)
+      return new Promise((resolve, reject) => {
+        sass.render(
+          {
+            data: content,
+            includePaths: ["src"],
+            sourceMap: true,
+            outFile: "x" // this is necessary, but is ignored
+          },
+          (err, result) => {
+            if (err) return reject(err)
 
-              resolve({
-                code: result.css.toString(),
-                map: result.map.toString()
-              })
-            }
-          )
-        })
-      },
-      typescript: preprocess({
+            resolve({
+              code: result.css.toString(),
+              map: result.map.toString()
+            })
+          }
+        )
+      })
+    },
+    typescript: preprocess({
       env,
       compilerOptions: {
         ...readConfigFile(env),
